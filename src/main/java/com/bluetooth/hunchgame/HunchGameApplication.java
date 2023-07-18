cf3afbbf177b37ec7b7a95c5102b9ff406367564
0gonge <challengef0802@gmail.com>
2023-07-18 21:45:16 +0900

    first

---
+++ src/main/java/com/bluetooth/hunchgame/HunchGameApplication.java
@@ -0,0 +1,13 @@
+package com.bluetooth.hunchgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunchGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HunchGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluetooth/hunchgame/HunchGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluetooth.hunchgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HunchGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
